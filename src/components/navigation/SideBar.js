7e92520e32352a1fa406d9a0b2c7afa75dbfd198
MaryanDehv <maryanmodev@gmail.com>
2023-05-20 13:25:29 +0100

    working on home page styles

--- src/components/navigation/SideBar.js
+++ src/components/navigation/SideBar.js
@@ -6,16 +6,20 @@ const SideBar = () => {
             <div className="side-bar-logo">
                 <Logo />
             </div>
-            <ul className="side-bar-list">
-                <li><span><ClockIcon /></span><span>Ongoing</span></li>
-                <li><span><CheckIcon /></span><span>Completed</span></li>
-                <li><span><ListIcon /></span><span>List</span></li>
-                <li><span><CogIcon /></span><span>Setitngs</span></li>
-                <li><span><AddIcon /></span><span>Add Book</span></li>
-            </ul>
-            <div className="side-bar-mode">
-                <div className="side-bar-light"><SunIcon /></div>
-                <div className="side-bar-dark"><MoonIcon /></div>
+            <div className="side-bar-content">
+                <ul className="side-bar-list">
+                    <li className="uppercase button flex v-center"><span><ClockIcon /></span><span>Ongoing</span></li>
+                    <li className="uppercase button flex v-center"><span><CheckIcon /></span><span>Completed</span></li>
+                    <li className="uppercase button flex v-center"><span><ListIcon /></span><span>List</span></li>
+                    <li className="uppercase button flex v-center"><span><CogIcon /></span><span>Setitngs</span></li>
+                    <li className="uppercase button flex v-center"><span><AddIcon /></span><span>Add Book</span></li>
+                </ul>
+                <div className="side-bar-mode-container flex h-center">
+                    <div className="side-bar-mode flex v-h-center">
+                        <div className="side-bar-light flex v-h-center selected"><SunIcon /></div>
+                        <div className="side-bar-dark flex v-h-center"><MoonIcon /></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/navigation/TopBar.js
+++ src/components/navigation/TopBar.js
@@ -2,12 +2,12 @@ import { BellIcon, Chatcon, ClockIcon } from "../../images/icons/customIcons";
 
 const TopBar = () => {
     return(
-        <div className="top-bar">
+        <div className="top-bar flex">
             <div className="top-bar-search">
             </div>
-            <div className="top-bar-account">
-                <p><ClockIcon /> Read for <strrong> 50h 30m</strrong> this week</p>
-                <div className="icons">
+            <div className="top-bar-account flex">
+                <p className="flex v-center"><ClockIcon /> <span className="opacity"> Read for </span> <strong> 50h 30m</strong> <span className="opacity">this week</span></p>
+                <div className="icons flex v-center">
                     <div className="message"><Chatcon /></div>
                     <div className="notification"><BellIcon /></div>
                     <div className="user"></div>
