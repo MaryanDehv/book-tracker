7d5edbc26fe8a7862c49c9e4e79d41eab452a530
MaryanDehv <maryanmodev@gmail.com>
2023-05-21 06:09:02 +0100

    updates

--- src/components/navigation/TopBar.js
+++ src/components/navigation/TopBar.js
@@ -1,10 +1,18 @@
-import { BellIcon, Chatcon, ClockIcon } from "../../images/icons/customIcons";
+import { BellIcon, Chatcon, ClockIcon, FilterIcon, SearchIcon } from "../../images/icons/customIcons";
 
 const TopBar = () => {
     return(
-        <div className="top-bar flex">
+        <div className="top-bar flex h-center">
             <div className="container flex">
                 <div className="top-bar-search">
+                    <div className="top-bar-search-container">
+                        <div className="search icon"><SearchIcon /></div>
+                        <input className="search" type="text" placeholder="THE RIGHTEOUS MIND" />
+                        <div className="filter icon"><FilterIcon /></div>
+                        <div className="filtered">
+                            <div className="filter-item"> Completed </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="top-bar-account flex">
                     <p className="flex v-center"><ClockIcon /> <span className="opacity"> Read for </span> <strong> 50h 30m</strong> <span className="opacity">this week</span></p>
