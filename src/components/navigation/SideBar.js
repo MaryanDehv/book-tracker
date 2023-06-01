d41d297a217777f475529d6b770b05422019b999
MaryanDehv <maryanmodev@gmail.com>
2023-06-01 21:16:46 +0100

    adding styles to mobile nav

--- src/components/navigation/SideBar.js
+++ src/components/navigation/SideBar.js
@@ -22,13 +22,18 @@ const SideBar = ({toggle}) => {
             <div className="side-bar-logo flex v-center">
                 <Logo />
             </div>
+            <div className="side-bar-mobile-nav-user-details flex v-center">
+                <div className="side-bar-avatar"><div className="side-bar-avatar-designs"></div></div>
+                <div className="side-bar-read-notice flex v-center"><ClockIcon /> <span className="opacity"> Read for </span> <strong> 50h 30m</strong> <span className="opacity">this week</span></div>
+                <p className="side-bar-user"> Luffy </p>
+            </div>
             <div className="side-bar-content flex v-center">
                 <ul className="side-bar-list">
                     <li className="uppercase button flex v-center"><span><ClockIcon /></span><span className="list-name">Ongoing</span></li>
                     <li className="uppercase button flex v-center"><span><CheckIcon /></span><span className="list-name">Completed</span></li>
                     <li className="uppercase button flex v-center"><span><ListIcon /></span><span className="list-name">List</span></li>
                     <li className="uppercase button flex v-center"><span><CogIcon /></span><span className="list-name">Setitngs</span></li>
-                    <li className="uppercase button flex v-center"><span><AddIcon /></span><span className="list-name">Add Book</span></li>
+                    <li className="uppercase button flex v-center red-button"><span><AddIcon /></span><span className="list-name">Add Book</span></li>
                 </ul>
                 <div className="side-bar-mode-container flex h-center">
                     <div className="side-bar-mode flex v-h-center">
