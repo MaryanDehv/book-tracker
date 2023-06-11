8f1494c39602f547998cc2e6e7f63655da0a67bc
MaryanDehv <maryanmodev@gmail.com>
2023-06-11 20:36:46 +0100

    working on logic for habit tracker

--- src/components/cards/HabitTracker.js
+++ src/components/cards/HabitTracker.js
@@ -1,11 +1,37 @@
 const HabitTracker = () => {
+    // will need to start based on when the user makes account - for now will start from 11/6
     function getBlocks(amount){
         let html = []
-        for(let i = 0 ; i < amount ; i++){
-            html.push(<div className="habit-blocks"></div>)
+
+        Date.prototype.addDays = function(days){
+            var date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date
         }
+
+        function getDates(startDate, stopDate) {
+            let arr = [];
+            let currentDate = startDate;
+            while(currentDate <= stopDate) {
+                arr.push({day: currentDate.getDate() ,  month: currentDate.getMonth() + 1 , isoString: currentDate.toISOString()})
+                currentDate = currentDate.addDays(1);
+            }
+            return arr;
+        }
+
+        const dates = getDates(new Date("2023-06-10T19:28:02.034Z") , (new Date()).addDays(amount))
+
+        for(let i = 0 ; i < amount ; i++){
+            html.push(<div className={`habit-blocks ${(new Date(dates[i].isoString)) < (new Date()) ? "yesterday" : "new"}`} onClick={colorBlock} date-date={dates[i].isoString}></div>)
+        }
+
         return html;
     }
+
+    function colorBlock(block){
+        block.currentTarget.classList.add("read")
+    }
+
     return(
         <>
             {
